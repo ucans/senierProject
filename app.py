f97c3126ac457ca9b98da6f3aa5f34cacff6efb0
Ucan <zyzy1231@naver.com>
2021-12-03 19:57:46 +0900

    Modify app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request
 from flask import send_from_directory
 
-
+# flask 객체 instance 생성
 app = Flask(__name__)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -12,25 +12,28 @@ STATIC_FOLDER = 'static'
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 # procesing uploaded file and predict it
-@app.route('/upload', methods=['POST','GET'])
+@app.route('/upload', methods=['POST', 'GET'])
 def upload_file():
     if request.method == 'GET':
         return render_template('index.html')
     else:
         file = request.files['image']
-        full_name = os.path.join(UPLOAD_FOLDER,file.filename)
+        full_name = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(full_name)
 
-    return render_template('predict.html', image_file_name = file.filename)
+    return render_template('predict.html', image_file_name=file.filename)
+
 
 @app.route('/uploads/<filename>')
 def send_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
+
 if __name__ == '__main__':
-    app.debug = True
-    app.run(debug=True)
-    app.debug = True
+    # 0.0.0.0 외부 접속 허용
+    app.run(host = '0.0.0.0', port = 80, debug=True)
+
