2537f8d0ef1da6069eef8e692c91aa8409c4eeb5
Ucan <zyzy1231@naver.com>
2021-12-10 17:30:17 +0900

    Add math_ocr

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import logging
 import os
+
+import cv2
+import numpy
 from flask import Flask, render_template, request
 from flask import send_from_directory
+from inference import LInference
+from PIL import Image
 
 # flask 객체 instance 생성
 app = Flask(__name__)
@@ -26,8 +31,13 @@ def upload_file():
         print('The dir path', dir_path)
         full_name = os.path.join(dir_path, UPLOAD_FOLDER, file.filename)
         file.save(full_name)
+        # 업로드한 사진 불러오기
+        read_img = Image.open(file)
+        # Latex 문자열로 변환
+        latex_data = inf.getLatext(read_img)[0]
+        # Todo : Latex 이미지로 변환
 
-    return render_template('predict.html', image_file_name=file.filename)
+    return render_template('predict.html', image_file_name=file.filename, latex_data=latex_data)
 
 
 @app.route('/uploads/<filename>')
@@ -36,5 +46,6 @@ def send_file(filename):
 
 
 if __name__ == '__main__':
+    inf = LInference()
     # 0.0.0.0 외부 접속 허용
     app.run(host = "0.0.0.0", port = 5000, debug=True)
