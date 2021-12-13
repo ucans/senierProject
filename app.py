c28e1cf0ccdbc5facfd1d20a3e1a610da60e8e7e
Ucan <zyzy1231@naver.com>
2021-12-13 20:12:23 +0900

    Add latex image

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import logging
 import os
-
-import cv2
-import numpy
 from flask import Flask, render_template, request
 from flask import send_from_directory
 from inference import LInference
 from PIL import Image
+from io import BytesIO
+import urllib.request
+from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse
 
 # flask 객체 instance 생성
 app = Flask(__name__)
@@ -14,6 +14,7 @@ app = Flask(__name__)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = 'uploads'
 STATIC_FOLDER = 'static'
+PREDICT_FOLDER = 'predicts'
 
 
 @app.route('/')
@@ -36,6 +37,8 @@ def upload_file():
         # Latex 문자열로 변환
         latex_data = inf.getLatext(read_img)[0]
         # Todo : Latex 이미지로 변환
+        predict_file = os.path.join(dir_path, PREDICT_FOLDER, file.filename)
+        save_latex_img(latex_data, predict_file)
 
     return render_template('predict.html', image_file_name=file.filename, latex_data=latex_data)
 
@@ -43,6 +46,23 @@ def upload_file():
 @app.route('/uploads/<filename>')
 def send_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
+
+
+@app.route('/predicts/<filename>')
+def send_latex_img(filename):
+    return send_from_directory(PREDICT_FOLDER, filename)
+
+
+def save_latex_img(latex_data, predict_file):
+    urlString = "http://www.sciweavers.org/tex2img.php?eq=%5Csqrt%5B3%5D%7Bx%5E3%2By%5E3%20%5Cover%202%7D&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0"
+    url = urlparse(urlString)
+    qs = dict(parse_qsl(url.query))
+    qs['eq'] = latex_data
+    parts = url._replace(query=urlencode(qs))
+    url = urlunparse(parts)
+
+    latex_hex_img = urllib.request.urlopen(url).read()
+    Image.open(BytesIO(latex_hex_img)).convert('RGB').save(predict_file)
 
 
 if __name__ == '__main__':
